57c40bc8255ed3e0abf7c8e498ea1c0df52d105b
LizaChelishev <lizachelishev@gmail.com>
2021-10-20 20:31:20 +0300

    done

---
+++ even_or_odd.py
@@ -0,0 +1,9 @@
+def even_or_odd(x):
+    if x % 2 == 0 :
+        print('Your number is even.')
+    else:
+        print('Your number is odd.')
+
+
+x = int(input('Enter a positive integer number: '))
+even_or_odd(x)
